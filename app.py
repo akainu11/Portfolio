6f083f17f994ee4693093496491150ecc3e3cdf3
Adam <adamnobunaga@gmail.com>
2021-09-06 21:54:10 -0700

    a

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 app.debug = True
 @app.route("/")
-def index():
+@app.route("/home")
+def home():
     return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
    app.run(debug=True) 
-
