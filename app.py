5eb06ec7c67e671363aa2d2661812da37eaf94cc
Adam <adamnobunaga@gmail.com>
2021-09-06 18:35:38 -0700

    d

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+#use 'source venv/bin/activate' to start virtual environment
+#use 'flask run' to test on local server
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
+app.debug = True
 @app.route("/")
 def index():
     return render_template('home.html')
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True) 
+
